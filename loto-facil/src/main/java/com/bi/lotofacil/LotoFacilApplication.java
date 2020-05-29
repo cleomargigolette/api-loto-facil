92f1aa947b4cb923b632e71cec1a826521eee8ff
cleomargigolette <you@example.com>
2020-05-28 21:36:28 -0300

    create: projeto api loto facil

---
+++ loto-facil/src/main/java/com/bi/lotofacil/LotoFacilApplication.java
@@ -0,0 +1,13 @@
+package com.bi.lotofacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotoFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotoFacilApplication.class, args);
+	}
+
+}
